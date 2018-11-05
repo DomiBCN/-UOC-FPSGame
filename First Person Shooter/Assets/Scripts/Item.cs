9fa7bf300e1c88ec16f20533f59a0ea9429a9cf6
alex <alexd.aluma@gmail.com>
2018-11-05 20:31:43 +0100

    Reload && Hit system && HUD(ammo need design but is functional) && Items

---
+++ First Person Shooter/Assets/Scripts/Item.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour {
+
+    float rotationTime = 3f;
+    bool itemUsed;
+
+	// Update is called once per frame
+	void Update () {
+        transform.parent.rotation *= Quaternion.Euler(0f, Time.deltaTime * 360 * 1.0f / rotationTime, 0f);
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            if(gameObject.tag == "Life")
+            {
+                itemUsed = other.gameObject.GetComponent<Shooter>().AddLife(30);
+            }
+            else if(gameObject.tag == "Ammo")
+            {
+                itemUsed = other.gameObject.GetComponent<Shooter>().AddAmmo(60);
+            }
+            else if(gameObject.tag == "Shield")
+            {
+                itemUsed = other.gameObject.GetComponent<Shooter>().AddShield(30);
+            }
+
+            //destroy the item just if it's been used
+            if (itemUsed)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
--- First Person Shooter/Assets/Scripts/Shooter.cs
+++ First Person Shooter/Assets/Scripts/Shooter.cs
@@ -1,16 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Shooter : MonoBehaviour
 {
     public GameObject decalPrefab;
-    public AudioSource fireSound;
-    public float life = 100;
+    public AudioSource gunAudioSource;
+    public AudioClip fireSound;
+    public AudioClip reloadSound;
     public float damageForce = 10f;
+    public Text cartridgeTxt;
+    public Text totalAmmunitionTxt;
+    public Text shieldTxt;
+    public Text lifeTxt;
+    public Slider shieldSlider;
+    public Slider lifeSlider;
+
+    float life = 150;
+    float shield = 5;
+    int totalAmmunition = 120;
+    int cartridgeAmmo = 30;
+
+    int maxAmmunition = 990;
+    float maxLife = 150;
+    float maxShield = 150;
 
     GameObject[] totalDecals;
     int actual_decal = 0;
+    bool reloading;
+
+    private void Awake()
+    {
+        cartridgeTxt.text = cartridgeAmmo.ToString();
+        totalAmmunitionTxt.text = totalAmmunition.ToString();
+        shieldTxt.text = shield.ToString();
+        lifeTxt.text = life.ToString();
+        shieldSlider.value = shield;
+        lifeSlider.value = life;
+    }
 
     private void Start()
     {
@@ -20,7 +48,7 @@ public class Shooter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !reloading)
         {
             RaycastHit hit;
 
@@ -29,7 +57,7 @@ public class Shooter : MonoBehaviour
                 Destroy(totalDecals[actual_decal]);
                 totalDecals[actual_decal] = GameObject.Instantiate(decalPrefab, hit.point + hit.normal * 0.01f, Quaternion.FromToRotation(Vector3.forward, -hit.normal), hit.collider.gameObject.transform);
 
-                fireSound.Play();
+                Shoot();
 
                 if (hit.collider.gameObject.tag == "Enemy")
                 {
@@ -41,11 +69,134 @@ public class Shooter : MonoBehaviour
 
             }
         }
+        if (Input.GetKeyDown(KeyCode.R) && !reloading)
+        {
+            reloading = true;
+            Reload();
+        }
     }
+
 
     public void Hit(float damage)
     {
-        life -= damage;
-        Debug.Log("Player hitted: " + life);
+        if (shield > 0)
+        {
+            shield -= damage;
+
+            if (shield < 0)
+            {
+                life += shield;
+                shield = 0;
+            }
+            shieldSlider.value = shield;
+            lifeSlider.value = life;
+
+            shieldTxt.text = shield.ToString();
+            lifeTxt.text = life.ToString();
+        }
+        else
+        {
+            life -= damage;
+            if (life < 0) life = 0;
+            lifeSlider.value = life;
+            lifeTxt.text = life.ToString();
+        }
+
     }
+
+    #region ITEMS
+    //If life or ammo are already at his maximum, do not get the items
+    public bool AddLife(float lifePlus)
+    {
+        bool useItem = false;
+        if (life < maxLife)
+        {
+            useItem = true;
+            life += lifePlus;
+            lifeSlider.value = life;
+            lifeTxt.text = life.ToString();
+        }
+        return useItem;
+    }
+
+    public bool AddShield(float shieldPlus)
+    {
+        bool useItem = false;
+        if (shield < maxShield)
+        {
+            useItem = true;
+            shield += shieldPlus;
+            shieldSlider.value = shield;
+            shieldTxt.text = shield.ToString();
+        }
+        return useItem;
+    }
+
+    public bool AddAmmo(int bullets)
+    {
+        bool useItem = false;
+        if (totalAmmunition < maxAmmunition)
+        {
+            useItem = true;
+            totalAmmunition = totalAmmunition + bullets > maxAmmunition ? maxAmmunition : totalAmmunition + bullets;
+            totalAmmunitionTxt.text = totalAmmunition.ToString();
+        }
+        return useItem;
+    }
+    #endregion
+
+    #region GUN
+    void Shoot()
+    {
+        Fire();
+        if (cartridgeAmmo == 1)
+        {
+            cartridgeAmmo = 0;
+            cartridgeTxt.text = cartridgeAmmo.ToString();
+            reloading = true;
+            //we want to let the fire clip to sound before start the reloading one
+            Invoke("Reload", 0.9f);
+        }
+        else
+        {
+            cartridgeAmmo -= 1;
+            cartridgeTxt.text = cartridgeAmmo.ToString();
+        }
+    }
+
+    void Fire()
+    {
+        gunAudioSource.clip = fireSound;
+        gunAudioSource.Play();
+    }
+
+    void Reload()
+    {
+        if (totalAmmunition > 0)
+        {
+            reloading = true;
+            gunAudioSource.clip = reloadSound;
+            gunAudioSource.Play();
+
+            //The reloading sound takes almost 3 seconds to complete
+            Invoke("UpdateAmmo", 1.5f);
+            Invoke("StopReloading", 3f);
+        }
+    }
+
+    void UpdateAmmo()
+    {
+        int bulletsNeeded = 30 - cartridgeAmmo;
+        cartridgeAmmo = totalAmmunition >= 30 ? cartridgeAmmo += bulletsNeeded : totalAmmunition;
+        totalAmmunition = Mathf.Clamp(totalAmmunition - bulletsNeeded, 0, maxAmmunition);
+
+        cartridgeTxt.text = cartridgeAmmo.ToString();
+        totalAmmunitionTxt.text = totalAmmunition.ToString();
+    }
+
+    void StopReloading()
+    {
+        reloading = false;
+    }
+    #endregion
 }
